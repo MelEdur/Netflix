c8db038a9fb90b0b0d83f20a342cc32887edde59
MelEdur <meleduardovi@gmail.com>
2024-10-07 19:57:01 -0300

    Añadido login y registro

---
+++ netflix/src/main/java/com/netflix/controller/ExploradorController.java
@@ -0,0 +1,22 @@
+package com.netflix.controller;
+
+import com.netflix.entity.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+public class ExploradorController {
+
+    @GetMapping("/explorador")
+    public ModelAndView vistaExplorador(Usuario usuario){
+
+        ModelAndView modelAndView = new ModelAndView("Vista explorador");
+        modelAndView.addObject("usuario",usuario);
+
+        return modelAndView;
+
+    }
+}
--- netflix/src/main/java/com/netflix/repository/IUsuarioRepository.java
+++ netflix/src/main/java/com/netflix/repository/IUsuarioRepository.java
@@ -4,6 +4,12 @@ import com.netflix.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario,Integer> {
+
+    boolean existsByCorreo(String correo);
+
+    Optional<Usuario> findByCorreo(String correo);
 }
